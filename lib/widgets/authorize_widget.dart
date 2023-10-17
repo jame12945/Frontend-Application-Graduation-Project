01466d1b79354f35c4683a2c2e7d6e9d40c6ca02
jame12945 <jame12945@gmail.com>
2023-10-17 14:27:32 +0700

    Can take Picture and Show

--- lib/widgets/authorize_widget.dart
+++ lib/widgets/authorize_widget.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+
 
 class AuthorizeWidget extends StatefulWidget {
   @override
@@ -27,6 +30,46 @@ class _AuthorizeWidgetState extends State<AuthorizeWidget> {
       setState(() {});
     }
   }
+  void captureAndSaveImage() async {
+    if (controller != null && controller!.value.isInitialized) {
+      // Wait for 2 seconds
+      await Future.delayed(Duration(seconds: 2));
+
+      try {
+        final path = '/storage/emulated/0/Android/data/com.example.bookingapp/files/GetPic';
+        final directory = Directory(path);
+        if (!directory.existsSync()) {
+          directory.createSync(recursive: true);
+        }
+
+        // Ensure that the camera is ready to take a picture
+        if (controller!.value.isTakingPicture) {
+          print("Camera is already taking a picture.");
+        } else {
+          final image = await controller!.takePicture();
+          final imageBytes = await image.readAsBytes();
+          final savedFile = File('$path/image.jpg');
+          await savedFile.writeAsBytes(imageBytes);
+          print("Image saved successfully.");
+          final imageFile = File('/storage/emulated/0/Android/data/com.example.bookingapp/files/GetPic/image.jpg');
+          if (imageFile.existsSync()) {
+            // มีภาพอยู่
+            print('มีภาพอยู่ในพาธ GetPic');
+          } else {
+            // ไม่มีภาพ
+            print('ไม่มีภาพในพาธ GetPic');
+          }
+
+        }
+      } catch (e) {
+        print("Error capturing and saving image: $e");
+      }
+    }
+  }
+
+
+
+
 
   @override
   void dispose() {
@@ -48,10 +91,17 @@ class _AuthorizeWidgetState extends State<AuthorizeWidget> {
         ),
         ElevatedButton(
           onPressed: () {
-            // Take a picture or perform any camera-related action
+            captureAndSaveImage();
           },
-          child: Text('Take Picture'),
+          child: Text("ถ่ายภาพ"),
         ),
+        Center(
+          child: Image.file(
+            File('/storage/emulated/0/Android/data/com.example.bookingapp/files/GetPic/image.jpg'),
+          ),
+        ),
+
+
       ],
     );
   }
