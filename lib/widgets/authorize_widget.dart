ea0a35214aa1854f020af756ba64c72f1c06b097
jame12945 <jame12945@gmail.com>
2023-10-19 16:46:19 +0700

    autometic capture

--- lib/widgets/authorize_widget.dart
+++ lib/widgets/authorize_widget.dart
@@ -4,7 +4,7 @@ import 'package:path_provider/path_provider.dart';
 import 'dart:io';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-
+import 'dart:async';
 
 class AuthorizeWidget extends StatefulWidget {
   @override
@@ -31,68 +31,60 @@ class _AuthorizeWidgetState extends State<AuthorizeWidget> {
     if (mounted) {
       setState(() {});
     }
+
+    // เรียกใช้ captureAndSaveImage เมื่อกล้องถูกเริ่มใช้งาน
+    captureAndSaveImage();
   }
-  void captureAndSaveImage() async {
-    if (controller != null && controller!.value.isInitialized) {
-      // Wait for 2 seconds
-      await Future.delayed(Duration(seconds: 2));
-
-      try {
-        final path = '/storage/emulated/0/Android/data/com.example.bookingapp/files/GetPic';
-        final directory = Directory(path);
-        if (!directory.existsSync()) {
-          directory.createSync(recursive: true);
-        }
-
-        // Ensure that the camera is ready to take a picture
-        if (controller!.value.isTakingPicture) {
-          print("Camera is already taking a picture.");
-        } else {
-          final image = await controller!.takePicture();
-          final imageBytes = await image.readAsBytes();
-          final savedFile = File('$path/image.jpg');
-          await savedFile.writeAsBytes(imageBytes);
-          print("Image saved successfully.");
-          final imageFile = File('/storage/emulated/0/Android/data/com.example.bookingapp/files/GetPic/image.jpg');
-          if (imageFile.existsSync()) {
-            // มีภาพอยู่
-            print('มีภาพอยู่ในพาธ GetPic');
-          } else {
-            // ไม่มีภาพ
-            print('ไม่มีภาพในพาธ GetPic');
+  void captureAndSaveImage() async{
+    if(controller!=null && controller!.value.isInitialized){
+      try{
+          final path = '/storage/emulated/0/Android/data/com.example.bookingapp/files/GetPic';
+          final directory = Directory(path);
+          if(!directory.existsSync()){
+            directory.createSync(recursive: true);
           }
-
-        }
-      } catch (e) {
-        print("Error capturing and saving image: $e");
+          if(controller!.value.isTakingPicture){
+            print("Camera is already taking a picture");
+          }
+          else{
+            final image = await controller!.takePicture();
+            final imageBytes = await image.readAsBytes();
+            final saveFile = File('$path/image.jpg');
+            await saveFile.writeAsBytes(imageBytes);
+            print("Image saved succesfully");
+            final imageFile = File('/storage/emulated/0/Android/data/com.example.bookingapp/files/GetPic/image.jpg');
+            if(imageFile.existsSync()){
+              print('Have Picture In Path');
+            }else{
+              print('Dont Have Picture In Path');
+            }
+          }
+      }
+      catch(e){
+        print("Error capyuring and saving image: $e");
       }
     }
   }
-//after facenet
-  Future<void> uploadImage(File imageFile) async {
-    final url = Uri.parse('YOUR_BACKEND_UPLOAD_URL'); // เปลี่ยน YOUR_BACKEND_UPLOAD_URL เป็น URL ของ backend ของคุณ
-    final request = http.MultipartRequest('POST', url);
-    request.files.add(
-      http.MultipartFile(
-        'image',
-        imageFile.readAsBytes().asStream(),
-        imageFile.lengthSync(),
-        filename: 'image.jpg',
-      ),
-    );
 
-    final response = await request.send();
-    if (response.statusCode == 200) {
-      print('อัปโหลดภาพสำเร็จ');
-    } else {
-      print('เกิดข้อผิดพลาดในการอัปโหลดภาพ: ${response.reasonPhrase}');
-    }
-  }
-
-//////////
-
-
-
+  // Future<void> uploadImage(File imageFile) async {
+  //   final url = Uri.parse('YOUR_BACKEND_UPLOAD_URL');
+  //   final request = http.MultipartRequest('POST', url);
+  //   request.files.add(
+  //     http.MultipartFile(
+  //       'image',
+  //       imageFile.readAsBytes().asStream(),
+  //       imageFile.lengthSync(),
+  //       filename: 'image.jpg',
+  //     ),
+  //   );
+  //
+  //   final response = await request.send();
+  //   if (response.statusCode == 200) {
+  //     print('อัปโหลดภาพสำเร็จ');
+  //   } else {
+  //     print('เกิดข้อผิดพลาดในการอัปโหลดภาพ: ${response.reasonPhrase}');
+  //   }
+  // }
 
   @override
   void dispose() {
@@ -112,19 +104,11 @@ class _AuthorizeWidgetState extends State<AuthorizeWidget> {
           aspectRatio: controller!.value.aspectRatio,
           child: CameraPreview(controller!),
         ),
-        ElevatedButton(
-          onPressed: () {
-            captureAndSaveImage();
-          },
-          child: Text("ถ่ายภาพ"),
-        ),
         Center(
           child: Image.file(
             File('/storage/emulated/0/Android/data/com.example.bookingapp/files/GetPic/image.jpg'),
           ),
         ),
-
-
       ],
     );
   }
