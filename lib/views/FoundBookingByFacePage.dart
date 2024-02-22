b9fbc798dc970a18f6cd55b45a21873da770321f
jame12945 <jame12945@gmail.com>
2024-02-23 01:06:58 +0700

    Success Project

--- lib/views/FoundBookingByFacePage.dart
+++ lib/views/FoundBookingByFacePage.dart
@@ -1,17 +1,65 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 
-class FoundBookingByFacePage extends StatelessWidget {
+class FoundBookingByFacePage extends StatefulWidget {
   final dynamic reservationData;
 
   FoundBookingByFacePage({required this.reservationData});
 
   @override
+  _FoundBookingByFacePageState createState() => _FoundBookingByFacePageState();
+}
+
+class _FoundBookingByFacePageState extends State<FoundBookingByFacePage> {
+  String doorStatus = 'Fetching...';
+
+  @override
+  void initState() {
+    super.initState();
+    closeDoor();
+  }
+
+  Future<void> fetchDoorStatus() async {
+    final response = await http.get(Uri.parse('http://192.168.1.10/status'));
+    if (response.statusCode == 200) {
+      setState(() {
+        doorStatus = response.body;
+      });
+    } else {
+      setState(() {
+        doorStatus = 'Error fetching status';
+      });
+    }
+  }
+
+  Future<void> closeDoor() async {
+    final response = await http.get(Uri.parse('http://192.168.1.10/open'));
+    if (response.statusCode == 200) {
+      fetchDoorStatus(); // Update door status after opening
+    } else {
+      // Handle error opening door
+    }
+  }
+
+  Future<void> openDoor() async {
+    final response = await http.get(Uri.parse('http://192.168.1.10/close'));
+    if (response.statusCode == 200) {
+      fetchDoorStatus();
+    } else {
+      // Handle error closing door
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
-    String DateReserve = reservationData['date_reservation'];
+    String DateReserve = widget.reservationData['date_reservation'];
     DateTime dateReserveDateTime = DateTime.parse(DateReserve);
     DateTime adjustedDateReserve = dateReserveDateTime.add(Duration(hours: 7));
     String formattedDate = DateFormat('yyyy-MM-dd').format(adjustedDateReserve);
+
+
+
 
     return Scaffold(
       body: Container(
@@ -29,25 +77,24 @@ class FoundBookingByFacePage extends StatelessWidget {
           padding: EdgeInsets.only(top: MediaQuery.of(context).size.height * 0.25),
           child: Card(
             color: Color.fromRGBO(255, 255, 255, 0.45),
-            margin: EdgeInsets.only(left: 35.0, right: 35.0,bottom: 220),
-
+            margin: EdgeInsets.only(left: 35.0, right: 35.0, bottom: 220),
             child: Column(
               children: [
                 ListTile(
                   title: Text("Reservation ID"),
-                  subtitle: Text("${reservationData["reservation_id"]}", style: TextStyle(color: Colors.black45)),
+                  subtitle: Text("${widget.reservationData["reservation_id"]}", style: TextStyle(color: Colors.black45)),
                   leading: Icon(Icons.confirmation_number, color: Colors.lightGreen),
                 ),
                 Divider(),
                 ListTile(
                   title: Text("Start Time"),
-                  subtitle: Text("${reservationData["start_time"]}", style: TextStyle(color: Colors.black45)),
+                  subtitle: Text("${widget.reservationData["start_time"]}", style: TextStyle(color: Colors.black45)),
                   leading: Icon(Icons.access_time, color: Colors.lightGreen),
                 ),
                 Divider(),
                 ListTile(
                   title: Text("End Time"),
-                  subtitle: Text("${reservationData["end_time"]}", style: TextStyle(color: Colors.black45)),
+                  subtitle: Text("${widget.reservationData["end_time"]}", style: TextStyle(color: Colors.black45)),
                   leading: Icon(Icons.access_time, color: Colors.lightGreen),
                 ),
                 Divider(),
@@ -59,19 +106,19 @@ class FoundBookingByFacePage extends StatelessWidget {
                 Divider(),
                 ListTile(
                   title: Text("Create"),
-                  subtitle: Text("${reservationData["create_reservlog"]}", style: TextStyle(color: Colors.black45)),
+                  subtitle: Text("${widget.reservationData["create_reservlog"]}", style: TextStyle(color: Colors.black45)),
                   leading: Icon(Icons.create, color: Colors.lightGreen),
                 ),
                 Divider(),
                 ListTile(
                   title: Text("Update"),
-                  subtitle: Text("${reservationData["update_reservlog"]}", style: TextStyle(color: Colors.black45)),
+                  subtitle: Text("${widget.reservationData["update_reservlog"]}", style: TextStyle(color: Colors.black45)),
                   leading: Icon(Icons.update, color: Colors.lightGreen),
                 ),
                 Divider(),
                 ListTile(
                   title: Text("Attendee list"),
-                  subtitle: Text("${reservationData["attendee_email"]}", style: TextStyle(color: Colors.black45)),
+                  subtitle: Text("${widget.reservationData["attendee_email"]}", style: TextStyle(color: Colors.black45)),
                   leading: Icon(Icons.people, color: Colors.lightGreen),
                 ),
                 Divider(),
@@ -86,5 +133,6 @@ class FoundBookingByFacePage extends StatelessWidget {
         ),
       ),
     );
+
   }
 }
